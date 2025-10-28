ec054b0b79d8fd811705e79d28d7b03b79243694
Luiz Felipe <zzzezizzz@gmail.com>
2025-10-28 11:03:27 -0300

    feat: implement toggle functionality for event images

--- Front/MainProject-App/src/app/components/eventos/eventos.component.ts
+++ Front/MainProject-App/src/app/components/eventos/eventos.component.ts
@@ -11,12 +11,18 @@ export class EventosComponent implements OnInit {
 
   widthImg: number = 150;
   marginImg: number = 2;
+  showImage: boolean = true;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
+
+  eventImage() {
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
       (response) => (this.eventos = response),
