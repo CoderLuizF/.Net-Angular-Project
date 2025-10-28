b463a056a80a653d92e4c95dfccd8dbfca977461
Luiz Felipe <zzzezizzz@gmail.com>
2025-10-28 07:45:11 -0300

    Seção4: Angular

---
+++ Back/src/MainProject.API/Controllers/EventosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MainProject.API.Data;
+using MainProject.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MainProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventosController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Teste Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Teste Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Teste Delete com id = {id}";
+        }
+
+
+    }
+}
--- Back/src/MainProject.API/Startup.cs
+++ Back/src/MainProject.API/Startup.cs
@@ -32,6 +32,7 @@ namespace MainProject.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MainProject.API", Version = "v1" });
@@ -53,6 +54,11 @@ namespace MainProject.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin()
+            );
 
             app.UseEndpoints(endpoints =>
             {
